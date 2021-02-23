b9310fa4a266beb7fc3848c00878da1fc3c8b6d1
Abdul Hamid Usman <Hazemidoo@gmail.com>
2021-02-23 17:16:58 +0100

    02d

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ if(navigator.geolocation){
               let {description} = data.weather[0];
               
               // testing
-              let icon = "13n";
+              let icon = "02d";
               
 
               // Set DOM Elements from the API
